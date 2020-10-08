791933f93329cf2c3a869a547c5e98da4ffd1beb
Ewelina <s165379@student.pg.edu.pl>
2020-10-08 15:17:22 +0200

    init

---
+++ src/test/java/com/ium/WarehouseServer/WarehouseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ium.WarehouseServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
